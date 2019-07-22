7caaa2daa85d7f6583ae133b03f3b8e758f521ba
z4rl <zpvedad.halcyondigital@gmail.com>
2019-07-22 16:05:12 +0800

    fix: practice profile tabs -> removed nuxt link

--- components/Profile/ProfileTabs.vue
+++ components/Profile/ProfileTabs.vue
@@ -1,27 +1,50 @@
 <template>
   <div>
     <div class="relative flex flex-nowrap">
-        <!-- @click.prevent="goTo('practice')" -->
+        <!-- @click.prevent="goTo('practice')" 
       <nuxt-link
         style="font-family:Nunito"
         to="/profile"
         class="mr-5 p-3 text-sm font-bold cursor-pointer no-underline"
         :class="$route.query.profile_tab === 'practice' ? 'border rounded-lg border-yellow-dark bg-yellow-dark' : 'text-grey-darker'"
       >Practice</nuxt-link>
-        <!-- @click.prevent="goTo('surgeries')" -->
+        @click.prevent="goTo('surgeries')"
       <nuxt-link
         style="font-family:Nunito"
         to="/profile/surgeries"
         class="mr-5 p-3 text-sm font-bold cursor-pointer no-underline"
         :class="$route.query.profile_tab === 'surgeries' ? 'border rounded-lg border-yellow-dark bg-yellow-dark' : 'text-grey-darker'"
       >Branches / Surgeries</nuxt-link>
-        <!-- @click.prevent="goTo('practiceDocuments')" -->
+        @click.prevent="goTo('practiceDocuments')" 
       <nuxt-link
         style="font-family:Nunito"
         to="/profile/documents"
         class="mr-5 p-3 text-sm font-bold cursor-pointer no-underline"
         :class="$route.query.profile_tab === 'practiceDocuments' ? 'border rounded-lg border-yellow-dark bg-yellow-dark' : 'text-grey-darker'"
-      >Practice Documents</nuxt-link>
+      >Practice Documents</nuxt-link> -->
+
+           
+      <div @click.prevent="goTo('practice')"
+        style="font-family:Nunito"
+        to="/profile"
+        class="mr-5 p-3 text-sm font-bold cursor-pointer no-underline"
+        :class="$route.query.profile_tab === 'practice' ? 'border rounded-lg border-yellow-dark bg-yellow-dark' : 'text-grey-darker'"
+      >Practice</div>
+     
+      <div @click.prevent="goTo('surgeries')"
+        style="font-family:Nunito"
+        to="/profile/surgeries"
+        class="mr-5 p-3 text-sm font-bold cursor-pointer no-underline"
+        :class="$route.query.profile_tab === 'surgeries' ? 'border rounded-lg border-yellow-dark bg-yellow-dark' : 'text-grey-darker'"
+      >Branches / Surgeries</div>
+        
+      <div @click.prevent="goTo('practiceDocuments')"
+        style="font-family:Nunito"
+        to="/profile/documents"
+        class="mr-5 p-3 text-sm font-bold cursor-pointer no-underline"
+        :class="$route.query.profile_tab === 'practiceDocuments' ? 'border rounded-lg border-yellow-dark bg-yellow-dark' : 'text-grey-darker'"
+      >Practice Documents</div>
+
     </div>
   </div>
 </template>
