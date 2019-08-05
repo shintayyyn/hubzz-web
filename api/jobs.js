00016781e57ad759f1f3d15db3992b74282ab348
Alvin <kpabad.halcyondigital@gmail.com>
2019-08-05 11:10:54 +0800

    ci: jobs/search

--- api/jobs.js
+++ api/jobs.js
@@ -23,7 +23,12 @@ export function fetchLocumJobs(axios, payload) {
     payload.status ? params.locum_status = payload.status : null
     payload.date_start ? params.date_start = payload.date_start : null
     payload.date_end ? params.date_end = payload.date_end : null
-
+    payload.shift_id ? params.shift_id = payload.shift_id : null
+    payload.rate ? params.rate = payload.rate : null
+    payload.locum_detail_rate_type_id ? params.locum_detail_rate_type_id = payload.locum_detail_rate_type_id : null
+    payload.near_post_code ? params.near_post_code = payload.near_post_code : null
+    payload.miles ? params.miles = payload.miles : null
+    payload.surgery_name ? params.surgery_name = payload.surgery_name : null
     return axios.$get(`/api/v1/locum/jobs${payload.countOnly ? '/count' : ''}`, { params: params })
 }
 
