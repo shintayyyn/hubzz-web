6a3e70db1ac12328b5ede039ec127ebd26db7b05
Mariel joy Mendoza <mbmendoza.halcyondigital@gmail.com>
2019-08-23 11:55:58 +0800

    fix: avatar display

--- components/Base/AppAvatar.vue
+++ components/Base/AppAvatar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative" :style="[{ width: width }, {height: height}]">
-    <img v-if="src" :src="src" class="object-cover rounded-full h-full" />
+    <img v-if="src" :src="src" class="object-cover rounded-full w-full h-full" />
     <img v-else src="https://via.placeholder.com/300/" class="object-cover rounded-full h-full" />
     <div v-if="type == 'update'" class="icon absolute top-0 right-0">
       <input
--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/sw.js?1566450289966', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/sw.js?1566531774301', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
