d56298eecbc5cb146ee97a7f753a34fb66718ca4
Alvin <kpabad.halcyondigital@gmail.com>
2019-10-30 13:50:43 +0800

    ci: pull mariel

---
+++ components/Sessions/JobPart/JobPartDetailModalParts.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="relative flex flex-col w-full mb-3">
+    <div class="text-xs sm:text-sm font-bold">Job Parts</div>
+    <AppTable
+      :total="total"
+      :items="parts"
+      :currentPage="current_page"
+      :perPage="params.limit"
+      :columns="columns"
+      :loading="loading"
+      :routerLink="routerLink"
+      @pagechanged="pagechanged"
+      @limitchanged="limitchanged"
+    />
+  </div>
+</template>
+<script>
+import AppTable from "@/components/Base/AppTable";
+export default {
+  components: {
+    AppTable
+  },
+
+  props: ["job_id"],
+  data() {
+    return {
+      parts: [],
+      current_page: 1,
+      // app table params
+      params: {
+        job_id: 0,
+        offset: 0,
+        limit: 5
+      },
+      total: 0,
+      // app table
+      columns: [
+        {
+          name: "Job Part Number",
+          dataIndex: "job_part_number",
+          class: "text-center"
+        },
+        {
+          name: "Date End",
+          dataIndex: "date_end",
+          class: "text-center"
+        },
+        {
+          name: "Date Start",
+          dataIndex: "date_start",
+          class: "text-center"
+        },
+        {
+          name: "Status",
+          dataIndex: "status",
+          class: "text-center"
+        }
+      ],
+      loading: false
+    };
+  },
+
+  computed: {
+    totalPages() {
+      return Math.ceil(this.total / this.perPage);
+    },
+    routerLink() {
+      let url = "/jobs";
+      if (this.$route.path.includes("related-jobs")) {
+        url = `/my-practice/${this.$route.params.practiceId}/related-jobs`;
+      }
+      return url;
+    }
+  },
+  created() {
+    this.params.job_id = this.job_id;
+    this.$axios
+      .$get(`/api/v1/locum/job-parts/count?job_id=${this.job_id}`)
+      .then(res => {
+        this.total = res.data.count;
+        this.getJobParts(this.params);
+      });
+  },
+  methods: {
+    getJobParts(params) {
+      this.loading = true;
+      this.$axios.$get(`/api/v1/locum/job-parts`, { params }).then(res => {
+        this.loading = false;
+        this.parts = res.data.job_parts;
+      });
+    },
+    pagechanged(page) {
+      this.current_page = page;
+      this.params.offset = this.params.limit * (page - 1);
+      this.getJobParts(this.params);
+    },
+    limitchanged(limit) {
+      this.current_page = 1;
+      this.params.offset = 0;
+      this.params.limit = limit;
+      this.getJobParts(this.params);
+    }
+  }
+};
+</script>
+<style scoped>
+.shield {
+  z-index: 511;
+}
+.modal-container {
+  z-index: 512;
+}
+@media screen and (min-width: 1200px) {
+  .modal-container {
+    width: 70%;
+  }
+}
+.wrapper {
+  transition: all 0.3s linear;
+}
+</style>
---
+++ components/Sessions/JobPartDetailModal.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="p-4 md:p-8">
+    <div @click="close" class="cursor-pointer">
+      <svgicon name="left-arrow" height="32" width="32" />
+    </div>
+    <div class="flex flex-row justify-start items-center my-3">
+      <div class="leading-loose font-bold text-md sm:text-lg">{{job_part.job.title}}</div>
+      <div
+        class="mx-2 text-sm sm:text-sm py-2 px-4 rounded font-semibold uppercase"
+        :class="bgStatus(job_part.locum_status)"
+      >{{job_part.locum_status}}</div>
+      <div
+        class="mx-2 text-sm sm:text-sm py-2 px-4 rounded font-semibold"
+        :class="jobPartStatus === 'Completed' ? 'bg-green-500' : 'bg-gray-300'"
+        v-if="job_part.locum_status === 'Completed'"
+      >{{jobPartStatus}}</div>
+    </div>
+    <div
+      class="text-xs sm:text-sm py-2"
+    >Posted {{$moment(job_part.date_created).format('DD/MM/YYYY')}}</div>
+    <div class="flex flex-row flex-wrap justify-start">
+      <div class="p-0 md:pr-4 w-full md:w-1/2">
+        <JobPartDetailModalInfo :job_part="job_part" />
+      </div>
+      <div class="p-0 md:pl-4 my-4 md:m-0 w-full md:w-1/2">
+        <div class="flex flex-col">
+          <JobPartDetailModalParts
+            :parts="job_part.job.job_parts"
+            :job_id="job_part.job.id"
+            v-if="job_part.job.job_parts.length > 1"
+          />
+          <JobDetailModalMap :job="job_part.job" />
+          <JobDetailModalUnassignForm
+            :job="job_part.job"
+            v-if="job_part.locum_status === 'Current'"
+            @close="close"
+            @removeJobPart="removeJobPart"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import JobPartDetailModalInfo from "@/components/Jobs/JobPart/JobPartDetailModalInfo";
+import JobPartDetailModalParts from "@/components/Jobs/JobPart/JobPartDetailModalParts";
+//
+import JobDetailModalInfo from "@/components/Jobs/JobDetailModalInfo";
+import JobDetailModalMap from "@/components/Jobs/JobDetailModalMap";
+import JobDetailModalUnassignForm from "@/components/Jobs/JobDetailModalUnassignForm";
+import JobDetailModalApplyForm from "@/components/Jobs/JobDetailModalApplyForm";
+import JobDetailModalCancelForm from "@/components/Jobs/JobDetailModalCancelForm";
+export default {
+  props: ["job_part"],
+
+  components: {
+    JobPartDetailModalInfo,
+    JobPartDetailModalParts,
+    //
+    JobDetailModalInfo,
+    JobDetailModalMap,
+    JobDetailModalUnassignForm,
+    JobDetailModalApplyForm,
+    JobDetailModalCancelForm
+  },
+  computed: {
+    jobPartStatus() {
+      let status = "TO BE INVOICED";
+      if (this.job_part.disputed) {
+        status = "DISPUTED";
+      }
+      if (this.job_part.invoiced && this.job_part.issued) {
+        status = "INVOICED";
+      }
+      return status;
+    }
+  },
+  methods: {
+    removeJobPart(id) {
+      this.$store.commit("jobs/REMOVE_LOCUM_ONGOING_JOB", id);
+    },
+    close() {
+      this.$emit("close");
+    },
+    bgStatus(status) {
+      switch (status) {
+        case "Available":
+          return "bg-yellow-500";
+          break;
+        case "Applied":
+          return "bg-orange-400 text-white";
+          break;
+        case "Completed":
+          return "bg-green-400 text-white";
+          break;
+        case "Allocated":
+          return "bg-green-400";
+          break;
+        default:
+          return "bg-red-500 text-white";
+      }
+    }
+  }
+};
+</script>
+
