1c436acd113ebdded29e8f762a4dc2f962910b07
z4rl <zpvedad.halcyondigital@gmail.com>
2019-11-22 19:38:21 +0800

    build: surgery management spoke permissions

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-importScripts('/sw.js?1574391665449', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
-=======
-importScripts('/sw.js?1574139877637', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
->>>>>>> d5f2df14b59273283da4500cf571a586d9c2c2f9
+importScripts('/sw.js?1574404040134', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
