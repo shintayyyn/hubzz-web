098f670b600aa7bc58acc908c446e6baa316cb74
z4rl <zpvedad.halcyondigital@gmail.com>
2019-12-02 19:03:45 +0800

    build: surgery banks for practice hub

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,4 +1,1 @@
-importScripts(
-	"/sw.js?1575028326314",
-	"https://cdn.onesignal.com/sdks/OneSignalSDK.js"
-);
+importScripts('/sw.js?1575268213765', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
