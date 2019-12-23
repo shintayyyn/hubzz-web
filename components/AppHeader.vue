938397fae49be8c801f049f7f1131104c05af687
z4rl <zpvedad.halcyondigital@gmail.com>
2019-12-23 08:57:02 +0800

    fix: create job button

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -115,11 +115,10 @@ export default {
   async created() {
     if (this.$auth.user.domain === "Practice") {
       if (this.$auth.user.practice_detail.practice.type === "Spoke") {
-        if (
-          this.$auth.user.practice_detail.practice
-            .allow_surgery_create_sessions == false
-        ) {
-          this.notAllowed = true;
+        if (this.$auth.user.practice_detail.practice.parent_practice_id) {
+          if (this.$auth.user.practice_detail.practice.allow_surgery_create_sessions == false) {
+            this.notAllowed = true;
+          }
         }
       }
     }
