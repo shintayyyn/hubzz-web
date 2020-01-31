c6bc205e62b66b30f588b14563e061e0192a127c
Alvin <kpabad.halcyondigital@gmail.com>
2020-01-31 08:06:51 +0800

    ci: fix

--- middleware/isAuthorized.js
+++ middleware/isAuthorized.js
@@ -1,7 +1,7 @@
 export default function ({ store, route, redirect }) {
   let practiceRoute = ['/profile', '/my-banks', '/sessions']
   let locumRoute = ['/compliance', '/availability', '/jobs']
-  let normalPracticeRoute = ['/my-banks', '/sessions', '/practice-billing']
+  let practiceHubType1Routes = ['/my-banks', '/sessions', '/practice-billing']
 
   let spokeRoutes = ['/dashboard/create', '/practice-billing/invoices-from-hubzz/', '/practice-billing', 'invoices-from-hubzz']
 
@@ -15,40 +15,30 @@ export default function ({ store, route, redirect }) {
     store.$auth.user.domain === 'Practice' &&
     store.$auth.user.practice_detail.practice.type === 'Hub' &&
     store.$auth.user.practice_detail.practice.hub_type === 'Type 2' &&
-    normalPracticeRoute.includes(route.path)) {
+    practiceHubType1Routes.includes(route.path)) {
     return redirect('/')
   }
+
   if (store.$auth.user &&
     store.$auth.user.domain === 'Practice' &&
-    store.$auth.user.practice_detail.practice.type === 'Spoke') {
+    store.$auth.user.practice_detail.practice.type === 'Spoke' && store.$auth.user.practice_detail.practice.parent_practice_id) {
 
-      if (store.$auth.user.practice_detail.practice.allow_surgery_create_sessions === false) {
-        if (route.path.includes('/dashboard/create')) {
-          return redirect('/')
-        }
+    if (store.$auth.user.practice_detail.practice.allow_surgery_create_sessions === false) {
+      if (route.path.includes('/dashboard/create')) {
+        return redirect('/')
       }
+    }
 
-      // if (store.$auth.user.practice_detail.practice.allow_surgery_bill_locum === false && 
-      //   store.$auth.user.practice_detail.practice.allow_surgery_bill_hubzz === true) {
-      //   if (route.path === '/practice-billing') {
-      //     return redirect('/practice-billing/invoices-from-hubzz/')
-      //   } 
-      // } else if (store.$auth.user.practice_detail.practice.allow_surgery_bill_locum === false) {
-      //   if (route.path.includes('/practice-billing')) {
-      //     return redirect('/')
-      //   }
-      // } 
-
-      if (store.$auth.user.practice_detail.practice.allow_surgery_bill_hubzz === false) {
-        if (route.path.includes('/practice-billing/invoices-from-hubzz')){
-          return redirect('/')
-        }
+    if (store.$auth.user.practice_detail.practice.allow_surgery_bill_hubzz === false) {
+      if (route.path.includes('/practice-billing/invoices-from-hubzz')) {
+        return redirect('/')
       }
+    }
 
-      if (store.$auth.user.practice_detail.practice.share_banks_to_other_surgeries === false) {
-        if (route.path.includes('/surgery-management/practice-hub/spoke-siblings')){
-          return redirect('/')
-        }
+    if (store.$auth.user.practice_detail.practice.share_banks_to_other_surgeries === false) {
+      if (route.path.includes('/surgery-management/practice-hub/spoke-siblings')) {
+        return redirect('/')
       }
+    }
   }
 }
