df661cff4144bef3f259a3c1543e09698fd1badc
z4rl <zpvedad.halcyondigital@gmail.com>
2020-02-18 18:59:55 +0800

    build: public permanent job

---
+++ pages/shared-permanent-job.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Public Permanent Job
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncDaa({ app, }) {
+    try {
+
+      return {
+
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/shared-permanent-job/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{sharedPermanentJob}}
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      sharedPermanentJob: '',
+    }
+  },
+  async asyncData({ app, route }) {
+    const response = await app.$axios.$get(`/api/v1/shared-permanent-job/${route.params.id}`)
+    const sharedPermanentJob = response.data.permanent_job
+    return {
+      sharedPermanentJob
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
