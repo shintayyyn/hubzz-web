7caed2ac811c58e3bba78f6dfeb8c55744730dcb
Alvin <kpabad.halcyondigital@gmail.com>
2020-02-25 14:21:40 +0800

    ci: fix

--- middleware/isNotAuthenticated.js
+++ middleware/isNotAuthenticated.js
@@ -1,6 +1,5 @@
 export default function ({ app, store, redirect }) {
    app.$auth.fetchUser()
-   console.log('test', app.$auth.loggedIn, store.$auth.$state.loggedIn)
    if (!store.$auth.$state.loggedIn) {
       redirect('/')
    }
