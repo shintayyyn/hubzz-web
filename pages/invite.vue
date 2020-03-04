830be9565d83fed297cdb58247a0d0dd6e098614
Arvi <cacheng.halcyondigital@gmail.com>
2020-03-04 15:41:25 +0800

    ...

--- pages/invite.vue
+++ pages/invite.vue
@@ -1,27 +1,42 @@
 <template>
   <section>
     <div class="flex flex-no-wrap justify-start">
-      <nuxt-link
-        :to="'/invite'"
-        class="mr-5 p-3 text-sm font-bold cursor-pointer"
-        :class="$route.name === 'invite' ? 'border rounded-lg border-yellow-500 bg-yellow-500' : 'text-gray-600'"
-      >Invite Locums</nuxt-link>
-      <nuxt-link
-        :to="'/invite/invite-practices'"
-        class="mr-5 p-3 text-sm font-bold cursor-pointer"
-        :class="$route.name === 'invite-invite-practices' ? 'border rounded-lg border-yellow-500 bg-yellow-500' : 'text-gray-600'"
-      >Invite Practices</nuxt-link>
+      <nuxt-link to="/invite" class="mr-5 p-3 text-sm font-bold cursor-pointer"
+                 :class="$route.name === 'invite' ? 'border rounded-lg border-yellow-500 bg-yellow-500' : 'text-gray-600'"
+      >
+        Invite
+        Locums
+      </nuxt-link>
+
+      <nuxt-link to="/invite/invite-practices" class="mr-5 p-3 text-sm font-bold cursor-pointer"
+                 :class="$route.name === 'invite-invite-practices' ? 'border rounded-lg border-yellow-500 bg-yellow-500' : 'text-gray-600'"
+      >
+        Invite Practices
+      </nuxt-link>
     </div>
-    <!-- <a
-      href="//wa.me/?text=arvi%40gmail.com%0AHave%20you%20heard%20the%20buzz%20about%20hubzz%3F%0AJoin%20hubzz%20at%20%20%0Ahubzz.co.uk"
-    >whatsapp link to chat</a>-->
+
     <div class="mt-3">
-      <nuxt-child />
+      <nuxt-child :referralCode="referralCode" />
     </div>
   </section>
 </template>
+
 <script>
-export default {
-  middleware: "isVerified"
-};
+  export default {
+    middleware: "isVerified",
+
+    data () {
+      return {
+        referralCode: '',
+      }
+    },
+
+    mounted () {
+      this.$axios.get('/api/v1/user-invites/referral-code').then((response) => {
+        if (response.data && response.data.data && response.data.data.user_referral_code) {
+          this.referralCode = response.data.data.user_referral_code.referral_code
+        }
+      })
+    },
+  }
 </script>
