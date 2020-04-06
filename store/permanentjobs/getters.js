b82767b88a84ec07fd910e73e616d87406f57b22
Zarley Vedad <zpvedad.halcyondigital@gmail.com>
2020-04-06 14:53:30 +0800

    building permaennt job realtime notifications

--- store/permanentjobs/getters.js
+++ store/permanentjobs/getters.js
@@ -1,3 +1,68 @@
-export default () => ({
-  
-})
+export default {
+  getLocumPermanentJobNotifications (state) {
+    let notifications = []
+    state.locum_permanent_job_notifications.forEach(notif => {
+      
+      let message = ''
+      let notifObj = null
+
+      switch (notif.notificationType) {
+        case 'Locum Notification Permanent Job Applied':
+          message = 'You have Applied to a Permanent Job'
+          break
+        case 'Locum Notification Permanent Job Invited':
+          message = 'You have been invited for an Interview for a Permanent Job Position'
+          break
+        case 'Locum Notification Permanent Job Rejected':
+          message = 'Your application for a Permanent Job position has been rejected'
+          break
+        default: 
+          message = ''
+      }
+
+      notifObj = {
+        ...notif,
+        id: notif.permanent_job.id,
+        status: notif.permanent_job_application.application_status,
+        permanent_job_status: notif.permanent_job.status,
+        permanent_job_app_id: notif.permanent_job_application.id,
+        type: 'Permanent Jobs',
+        message: `${message}. ${notif.locum_status === 'Pending'? 'This Permanent Job is no longer available.' : ''}`
+      }
+      notifications.push(notifObj)
+    })
+    console.log('notif', notifications)
+    return notifications
+  },
+
+  getPracticePermanentJobNotifications (state) {
+    let notifications = []
+    state.practice_permanent_job_notifications.forEach(notif => {
+      let message = ''
+      let notifObj = null
+
+      switch (notif.notificationType) {
+        case 'Practice Notification Permanent Job Applied':
+          message = 'A Locum has applied to a Permanent Job'
+          break
+        case 'Practice Notification Permanent Job Invited': 
+          message = 'You have invited a Locum for a Permanent Job Position'
+          break
+        default:
+          message = ''
+      }
+
+      notifObj = {
+        ...notif,
+        id: notif.permanent_job.id,
+        status: notif.permanent_job.job_posting_status,
+        application_status: notif.permanent_job_application.application_status,
+        permanent_job_app_id: notif.permanent_job_application.id,
+        type: 'Permanent Jobs',
+        message,
+      }
+      notifications.push(notifObj)
+    })
+    return notifications
+  },
+}
