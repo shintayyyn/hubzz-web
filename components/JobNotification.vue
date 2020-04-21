42fe1d7b796e43f10f4703a108418e75077694c9
Arvi <cacheng.halcyondigital@gmail.com>
2020-04-22 05:44:03 +0800

    ...

--- components/JobNotification.vue
+++ components/JobNotification.vue
@@ -110,7 +110,7 @@
     data () {
       return {
         toggleNotification: true,
-        showFocus: false
+        showFocus: false,
       }
     },
 
@@ -486,9 +486,11 @@
     margin: 50px 20px 0;
     padding: 0 4px 10px;
   }
+
   .notifications:hover .cards {
     opacity: 1;
   }
+
   .notifications::-webkit-scrollbar {
     display: none;
   }
