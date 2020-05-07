dc82e90be290a18df677915d946a28d4a292da8e
Arvi <cacheng.halcyondigital@gmail.com>
2020-05-07 17:11:45 +0800

    ...

--- pages/locum-billing/invoices/_id/index.vue
+++ pages/locum-billing/invoices/_id/index.vue
@@ -9,12 +9,16 @@
           <svgicon name="left-arrow" height="32" width="32" />
         </nuxt-link>
       </div>
-      <LocumPlatformBillingInvoiceForm :propInvoice="invoice" />
+      <LocumPlatformBillingInvoiceForm
+        :propInvoice="invoice"
+        :propJobPart="null"
+        @updateInvoice="$emit('updateInvoice', $event), $router.push({ name: 'locum-billing-invoices', query: {...$route.query} })"
+      />
     </div>
   </div>
 </template>
 <script>
-import LocumPlatformBillingInvoiceForm from "@/components/Billing/LocumPlatformBillingInvoiceForm";
+import LocumPlatformBillingInvoiceForm from "@/components/Billing/LocumPlatformBillingInvoiceForm"
 export default {
   transition: {
     name: "slide",
@@ -23,30 +27,30 @@ export default {
   components: {
     LocumPlatformBillingInvoiceForm
   },
-  async asyncData({ app, error, params }) {
+  async asyncData ({ app, error, params }) {
     try {
       const response = await app.$axios.get(
         `/api/v1/locum/locum-invoices/${params.id}`
-      );
+      )
 
       const invoice =
         response.data && response.data.data && response.data.data.locum_invoice
           ? response.data.data.locum_invoice
-          : null;
+          : null
 
       return {
         invoice
-      };
+      }
     } catch (err) {
       if (err && err.response.status === 404) {
-        return error({ status: 404, message: "This page could not be found" });
+        return error({ status: 404, message: "This page could not be found" })
       } else if (err & (err.response.status === 500)) {
-        return error({ status: 500, message: "Something went wrong!" });
+        return error({ status: 500, message: "Something went wrong!" })
       }
-      throw err;
+      throw err
     }
   }
-};
+}
 </script>
 
 <style scoped>
