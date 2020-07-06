dbebc5866c925251e39b73c076ad69c9289b1b1f
Arvi <cacheng.halcyondigital@gmail.com>
2020-07-06 16:23:39 +0800

    ...

--- components/AppSideBar.vue
+++ components/AppSideBar.vue
@@ -5,7 +5,9 @@
         <button
           class="close-button cursor-pointer focus:outline-none text-2xl font-bold text-yellow-500 px-4"
           @click="close"
-        >X</button>
+        >
+          X
+        </button>
 
         <div v-for="(item, index) in lists" :key="index" class="text-sm relative">
           <span
@@ -59,119 +61,119 @@
 </template>
 
 <script>
-import AppConfirmationModal from "@/components/Base/AppConfirmationModal";
+import AppConfirmationModal from "@/components/Base/AppConfirmationModal"
 
 export default {
   components: {
     AppConfirmationModal
   },
 
-  data() {
+  data () {
     return {
       signout_modal: false,
       confirmation_modal: false,
       lists: [],
       eligibleToSpoke: false
-    };
+    }
   },
 
   computed: {
-    authPermissions() {
-      return this.$store.getters["permissions"];
+    authPermissions () {
+      return this.$store.getters["permissions"]
     },
-    view_locum_jobs() {
-      return this.$store.getters["getViewLocumJobs"];
+    view_locum_jobs () {
+      return this.$store.getters["getViewLocumJobs"]
     },
-    view_permanent_jobs() {
-      return this.$store.getters["getViewPermanentJobs"];
+    view_permanent_jobs () {
+      return this.$store.getters["getViewPermanentJobs"]
     }
   },
 
   watch: {
-    view_locum_jobs() {
-      this.getInit();
+    view_locum_jobs () {
+      this.getInit()
     },
-    view_permanent_jobs() {
-      this.getInit();
+    view_permanent_jobs () {
+      this.getInit()
     }
   },
 
-  async created() {
+  async created () {
     if (
       this.$auth.loggedIn &&
       this.$auth.user.domain === "Practice" &&
       this.$auth.user.practice_detail.practice.type === "Stand Alone" &&
       this.authPermissions.includes("View Surgery Management")
     ) {
-      let hasHubInvitation = false;
-      let hasSpokeInvitation = false;
+      let hasHubInvitation = false
+      let hasSpokeInvitation = false
 
       await this.$axios
         .$get(`/api/v1/practice/me/parent-surgery/invitations-count`)
         .then(res => {
-          hasHubInvitation = res.data.count > 0;
-        });
+          hasHubInvitation = res.data.count > 0
+        })
 
       await this.$axios
         .$get(`/api/v1/practice/me/practice-surgeries/spoke-invitations/count`)
         .then(res => {
-          hasSpokeInvitation = res.data.count > 0;
-        });
+          hasSpokeInvitation = res.data.count > 0
+        })
 
-      this.eligibleToSpoke = hasHubInvitation || hasSpokeInvitation;
-      this.getInit();
+      this.eligibleToSpoke = hasHubInvitation || hasSpokeInvitation
+      this.getInit()
     } else {
-      this.getInit();
-      this.$store.dispatch("getViewJobsPermissions");
-      console.log(this.view_locum_jobs);
-      console.log(this.view_permanent_jobs);
+      this.getInit()
+      this.$store.dispatch("getViewJobsPermissions")
+      console.log(this.view_locum_jobs)
+      console.log(this.view_permanent_jobs)
     }
   },
 
-  mounted() {
+  mounted () {
     this.$loggedOutBroadcastChannel.addEventListener(
       "message",
       this.loggedOutHandler
-    );
-    this.addSocketListener();
+    )
+    this.addSocketListener()
   },
 
-  destroyed() {
+  destroyed () {
     this.$loggedOutBroadcastChannel.removeEventListener(
       "message",
       this.loggedOutHandler
-    );
-    this.removeSocketListener();
+    )
+    this.removeSocketListener()
   },
 
   methods: {
-    addSocketListener() {
+    addSocketListener () {
       this.$socket.removeListener(
         "Practice Notification Update Profile",
         this.updatePermissions
-      );
+      )
       this.$socket.removeListener(
         "Practice Notification Delete Profile",
         this.toggleConfirmationModal
-      );
+      )
     },
 
-    removeSocketListener() {
+    removeSocketListener () {
       this.$socket.removeListener(
         "Practice Notification Update Profile",
         this.updatePermissions
-      );
+      )
       this.$socket.removeListener(
         "Practice Notification Delete Profile",
         this.toggleConfirmationModal
-      );
+      )
     },
 
-    toggleConfirmationModal() {
-      this.confirmation_modal = true;
+    toggleConfirmationModal () {
+      this.confirmation_modal = true
     },
 
-    updatePermissions(user) {
+    updatePermissions (user) {
       if (
         user &&
         user.practice_detail &&
@@ -181,40 +183,40 @@ export default {
         this.$store.commit(
           "SET_PERMISSIONS",
           user.practice_detail.role.permissions
-        );
+        )
       } else {
-        this.$store.commit("SET_PERMISSIONS", []);
+        this.$store.commit("SET_PERMISSIONS", [])
       }
     },
 
-    getInit() {
-      const user = this.$auth.user;
+    getInit () {
+      const user = this.$auth.user
 
-      const { domain, status: accountStatus } = user;
+      const { domain, status: accountStatus } = user
 
       if (domain === "Locum") {
-        const locumTabList = [];
+        const locumTabList = []
 
         locumTabList.push({
           name: "Dashboard",
           route: "/dashboard"
-        });
+        })
 
         locumTabList.push({
           name: "Account",
           route: "/account"
-        });
+        })
 
         locumTabList.push({
           name: "Compliance",
           route: "/compliance"
-        });
+        })
 
         if (this.view_locum_jobs) {
           locumTabList.push({
             name: "Availability",
             route: "/availability"
-          });
+          })
         }
 
         if (
@@ -224,7 +226,7 @@ export default {
           locumTabList.push({
             name: "My Practice",
             route: "/my-practice"
-          });
+          })
         }
 
         if (
@@ -234,7 +236,7 @@ export default {
           locumTabList.push({
             name: "Jobs",
             route: "/jobs"
-          });
+          })
         }
 
         if (
@@ -244,7 +246,7 @@ export default {
           locumTabList.push({
             name: "Permanent Jobs",
             route: "/permanent-jobs"
-          });
+          })
         }
 
         if (
@@ -254,75 +256,75 @@ export default {
           locumTabList.push({
             name: "Billing",
             route: "/locum-billing"
-          });
+          })
         }
 
         if (["Active", "Dormant"].includes(accountStatus)) {
           locumTabList.push({
             name: "Reports",
             route: "/locum-reports"
-          });
+          })
         }
 
         locumTabList.push({
           name: "Invite",
           route: "/invite"
-        });
+        })
 
         locumTabList.push({
           name: "FAQ",
           route: "/faq"
-        });
+        })
 
         locumTabList.push({
           name: "Terms and Conditions",
           route: "/terms-and-conditions"
-        });
+        })
 
         locumTabList.push({
           name: "Contact Us",
           route: "/contact-us"
-        });
+        })
 
-        this.lists = locumTabList;
-        return;
+        this.lists = locumTabList
+        return
       }
 
       if (domain === "Practice") {
-        const practiceUser = this.$auth.user;
+        const practiceUser = this.$auth.user
 
         const practice = practiceUser.practice_detail
           ? practiceUser.practice_detail.practice
-          : null;
+          : null
 
         const {
           status: practiceStatus = null,
           type: practiceType = null,
           hub_type: hubType = null
-        } = practice || {};
+        } = practice || {}
 
-        const practiceTabList = [];
+        const practiceTabList = []
 
         practiceTabList.push({
           name: "Dashboard",
           route: "/dashboard"
-        });
+        })
 
         practiceTabList.push({
           name: "Account",
           route: "/account"
-        });
+        })
 
         if (this.authPermissions.includes("View Profile Practice")) {
           practiceTabList.push({
             name: "Profile",
             route: "/profile"
-          });
+          })
         } else if (this.authPermissions.includes("View Profile Users")) {
           practiceTabList.push({
             name: "Profile",
             route: "/profile/users"
-          });
+          })
         }
 
         if (
@@ -334,7 +336,7 @@ export default {
           practiceTabList.push({
             name: "Surgery Management",
             route: "/hub-surgery-management"
-          });
+          })
         }
 
         if (
@@ -346,7 +348,7 @@ export default {
           practiceTabList.push({
             name: "Surgery Management",
             route: "/spoke-surgery-management"
-          });
+          })
         }
 
         if (
@@ -359,7 +361,7 @@ export default {
           practiceTabList.push({
             name: "Surgery Management",
             route: "/spoke-surgery-management"
-          });
+          })
         }
 
         if (
@@ -370,7 +372,7 @@ export default {
           practiceTabList.push({
             name: "My Banks",
             route: "/my-banks"
-          });
+          })
         }
 
         if (
@@ -382,7 +384,7 @@ export default {
           practiceTabList.push({
             name: "Sessions",
             route: "/sessions"
-          });
+          })
         }
 
         if (
@@ -393,7 +395,7 @@ export default {
           practiceTabList.push({
             name: "Permanent Jobs",
             route: "/permanent-jobs"
-          });
+          })
         }
 
         if (
@@ -406,7 +408,7 @@ export default {
           practiceTabList.push({
             name: "Billing",
             route: "/practice-billing"
-          });
+          })
         }
 
         if (
@@ -416,13 +418,13 @@ export default {
           practiceTabList.push({
             name: "Reports",
             route: "/practice-reports"
-          });
+          })
         }
 
         practiceTabList.push({
           name: "Invite",
           route: "/invite"
-        });
+        })
 
         if (
           accountStatus === "Active" &&
@@ -432,96 +434,96 @@ export default {
           practiceTabList.push({
             name: "Roles and Permissions",
             route: "/roles-and-permissions"
-          });
+          })
         }
 
         practiceTabList.push({
           name: "FAQ",
           route: "/faq"
-        });
+        })
 
         practiceTabList.push({
           name: "Terms and Conditions",
           route: "/terms-and-conditions"
-        });
+        })
 
         practiceTabList.push({
           name: "Contact Us",
           route: "/contact-us"
-        });
+        })
 
-        this.lists = practiceTabList;
-        return;
+        this.lists = practiceTabList
+        return
       }
     },
 
-    logout() {
+    logout () {
       this.$axios
         .post("/api/v1/logout")
         .then(() => {
-          this.$store.commit("billing/CLEAR_PRACTICE_BILLING_NOTIFICATION");
-          this.$store.commit("billing/CLEAR_LOCUM_BILLING_NOTIFICATION");
-          this.$store.commit("jobs/CLEAR_PRACTICE_JOB_NOTIFICATION");
-          this.$store.commit("jobs/CLEAR_LOCUM_JOB_NOTIFICATION");
-          console.log("Socket Logged Out");
-          console.log("One Signal Logged Out");
+          this.$store.commit("billing/CLEAR_PRACTICE_BILLING_NOTIFICATION")
+          this.$store.commit("billing/CLEAR_LOCUM_BILLING_NOTIFICATION")
+          this.$store.commit("jobs/CLEAR_PRACTICE_JOB_NOTIFICATION")
+          this.$store.commit("jobs/CLEAR_LOCUM_JOB_NOTIFICATION")
+          console.log("Socket Logged Out")
+          console.log("One Signal Logged Out")
         })
         .catch(err => {
-          console.log("err", err.response || err);
+          console.log("err", err.response || err)
           if (err.response.data.message) {
             this.$store.commit("SET_NOTIFICATION", {
               enabled: true,
               status: "danger",
               text: [`${err.response.data.message}`]
-            });
+            })
           }
         })
         .finally(() => {
-          this.$emit("modal", false);
-          this.$store.commit("TOGGLE_SIDEBAR", false);
-          return this.loggedOutHandler();
+          this.$emit("modal", false)
+          this.$store.commit("TOGGLE_SIDEBAR", false)
+          return this.loggedOutHandler()
         })
         .then(() => {
-          this.$loggedOutBroadcastChannel.postMessage();
+          this.$loggedOutBroadcastChannel.postMessage()
         })
         .catch(err => {
-          console.log("err", err.response || err);
+          console.log("err", err.response || err)
           if (err.response.data.message) {
             this.$store.commit("SET_NOTIFICATION", {
               enabled: true,
               status: "danger",
               text: [`${err.response.data.message}`]
-            });
+            })
           }
-        });
+        })
     },
 
-    async loggedOutHandler() {
+    async loggedOutHandler () {
       try {
-        await this.$auth.logout();
-        this.$auth.$storage.setUniversal("_token.local", "");
-        this.$router.push("/");
+        await this.$auth.logout()
+        this.$auth.$storage.setUniversal("_token.local", "")
+        this.$router.push("/")
       } catch (err) {
-        console.log("err", err);
+        console.log("err", err)
       }
     },
 
-    async confirm() {
-      await this.$auth.logout();
-      this.$auth.$storage.setUniversal("_token.local", "");
-      this.$router.push("/");
+    async confirm () {
+      await this.$auth.logout()
+      this.$auth.$storage.setUniversal("_token.local", "")
+      this.$router.push("/")
     },
 
-    isDisabled(routeName) {
-      return this.$route.path.includes(routeName) ? "" : "click";
+    isDisabled (routeName) {
+      return this.$route.path.includes(routeName) ? "" : "click"
     },
 
-    close() {
-      this.$store.commit("TOGGLE_SIDEBAR", false);
-      document.body.style.overflow = "auto";
+    close () {
+      this.$store.commit("TOGGLE_SIDEBAR", false)
+      document.body.style.overflow = "auto"
     }
   }
-};
+}
 </script>
 
 <style scoped>
