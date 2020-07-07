1cc5679c2c3e596d85c350b1b2877406a143b842
Arvi <cacheng.halcyondigital@gmail.com>
2020-07-07 11:50:09 +0800

    ...

--- pages/locum-job-parts/index/_jobPartId.vue
+++ pages/locum-job-parts/index/_jobPartId.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="modal-container shadow-lg">
     <JobDetailModalAppointment
-      v-if="!activeJobTypePlatform"
+      v-if="jobPart && jobPart.job && jobPart.job.type === 'Private'"
       :job="jobPart.job"
       @close="close"
       @appointmentUpdated="$emit('appointmentUpdated')"
     />
-    <JobPartDetailModal v-if="activeJobTypePlatform" :job_part="jobPart" @close="close" />
+
+    <JobPartDetailModal v-if="jobPart && jobPart.job && jobPart.job.type === 'Platform'" :job_part="jobPart" @close="close" />
   </div>
 </template>
 
@@ -16,8 +17,8 @@ import JobDetailModalAppointment from "@/components/Jobs/JobDetailModalAppointme
 
 export default {
   transition: {
-    name: "slide",
-    mode: "out-in",
+    name: 'slide',
+    mode: 'out-in',
   },
 
   components: {
@@ -31,21 +32,13 @@ export default {
     }
   },
 
-  computed: {
-    activeJobTypePlatform () {
-      return this.jobPart && this.jobPart.job.type === "Platform"
-    },
-  },
-
   async asyncData ({ app, params, error, }) {
     try {
       const { jobPartId, } = params
 
-      let response = await app.$axios.get(
-        `/api/v1/locum/job-parts/${jobPartId}`
-      )
+      const response = await app.$axios.get(`/api/v1/locum/job-parts/${jobPartId}`)
 
-      let jobPart = response.data.data.job_part
+      const jobPart = response.data.data.job_part
 
       return {
         jobPart,
@@ -67,7 +60,7 @@ export default {
       const { query, } = this.$route
 
       this.$router.push({
-        name: "locum-job-parts-index",
+        name: 'locum-job-parts-index',
         query,
       })
     },
@@ -76,13 +69,13 @@ export default {
 </script>
 
 <style scoped>
-.modal-container {
-  z-index: 510;
-}
-
-@media screen and (min-width: 1200px) {
   .modal-container {
-    width: 90%;
+    z-index: 510;
   }
-}
+
+  @media screen and (min-width: 1200px) {
+    .modal-container {
+      width: 90%;
+    }
+  }
 </style>
