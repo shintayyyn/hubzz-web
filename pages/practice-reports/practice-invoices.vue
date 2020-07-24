637141e80c90dfdc1f6041eac26b7452a3fc3c3e
Zarley Vedad <zpveded.halcyondigital@gmail.com>
2020-07-24 17:49:04 +0800

    fixes

--- pages/practice-reports/practice-invoices.vue
+++ pages/practice-reports/practice-invoices.vue
@@ -121,310 +121,305 @@
 </template>
 
 <script>
-  import ReportTable from '@/components/Reports/ReportTable'
-  import ReportPagination from '@/components/Reports/ReportPagination'
-  import AppButton from '@/components/Base/AppButton'
-  import AppDate from '@/components/Base/AppDate'
-  export default {
-    components: {
-      ReportTable,
-      ReportPagination,
-      AppButton,
-      AppDate,
+import ReportTable from '@/components/Reports/ReportTable'
+import ReportPagination from '@/components/Reports/ReportPagination'
+import AppButton from '@/components/Base/AppButton'
+import AppDate from '@/components/Base/AppDate'
+export default {
+  components: {
+    ReportTable,
+    ReportPagination,
+    AppButton,
+    AppDate,
+  },
+
+  data () {
+    return {
+      loading: false,
+      downloading: false,
+      count: 0,
+      practiceInvoices: [],
+      orderBy: [],
+      orderBys: [
+        {
+          title: 'Practice Name (Ascending)',
+          column: 'practice_name',
+          direction: 'asc',
+        },
+        {
+          title: 'Practice Name (Descending)',
+          column: 'practice_name',
+          direction: 'desc',
+        },
+      ],
+      limit: 10,
+      limits: [
+        1,
+        2,
+        3,
+        4,
+        5,
+        10,
+        15,
+        20,
+        25,
+      ],
+      activePage: 1,
+      dateStart: '',
+      dateEnd: '',
+    }
+  },
+
+  computed: {
+    offset () {
+      return this.activePage * this.limit - this.limit
     },
 
-    data () {
-      return {
-        loading: false,
-        downloading: false,
-        count: 0,
-        practiceInvoices: [],
-        orderBy: [],
-        orderBys: [
-          {
-            title: 'Practice Name (Ascending)',
-            column: 'practice_name',
-            direction: 'asc',
-          },
-          {
-            title: 'Practice Name (Descending)',
-            column: 'practice_name',
-            direction: 'desc',
-          },
-        ],
-        limit: 10,
-        limits: [
-          1,
-          2,
-          3,
-          4,
-          5,
-          10,
-          15,
-          20,
-          25,
-        ],
-        activePage: 1,
-        dateStart: '',
-        dateEnd: '',
+    columnDetails () {
+      return [
+        {
+          title: '#',
+          key: 'index',
+          sort_key: null,
+          column: (item, index) => this.offset + index + 1,
+          justify: 'end',
+          flexGrow: 0,
+          flexShrink: 0,
+        },
+        {
+          title: 'Practice',
+          key: 'practice_name',
+          sort_key: 'practice_name',
+          column: (item) => item.practice_name,
+          justify: 'start',
+          flexGrow: 1,
+          flexShrink: 0,
+        },
+        {
+          title: 'Date Start',
+          key: 'date_start',
+          sort_key: 'date_start',
+          column: (item) => this.$moment(item.date_start, 'YYYY-MM-DD').format('DD/MM/YYYY'),
+          justify: 'center',
+          flexGrow: 1,
+          flexShrink: 0,
+        },
+        {
+          title: 'Date End',
+          key: 'date_end',
+          sort_key: 'date_end',
+          column: (item) => this.$moment(item.date_end, 'YYYY-MM-DD').format('DD/MM/YYYY'),
+          justify: 'center',
+          flexGrow: 1,
+          flexShrink: 0,
+        },
+        {
+          title: 'Invoice Number',
+          key: 'invoice_number',
+          sort_key: 'invoice_number',
+          column: (item) => item.invoice_number,
+          justify: 'start',
+          flexGrow: 1,
+          flexShrink: 0,
+        },
+        {
+          title: '£ Amount',
+          key: 'total_amount',
+          sort_key: 'total_amount',
+          column: (item) => item.total_amount ? item.total_amount.toFixed(2) : null,
+          justify: 'end',
+          flexGrow: 1,
+          flexShrink: 0,
+        },
+        {
+          title: 'Hours',
+          key: 'hours',
+          sort_key: 'hours',
+          column: (item) => item.hours_with_minutes,
+          justify: 'end',
+          flexGrow: 1,
+          flexShrink: 0,
+        },
+        {
+          title: 'Discount',
+          key: 'total_credit',
+          sort_key: 'total_credit',
+          column: (item) => item.total_credit ? item.total_credit.toFixed(2) : null,
+          justify: 'end',
+          flexGrow: 1,
+          flexShrink: 0,
+        },
+      ]
+    },
+
+    pages () {
+      return Math.max(Math.ceil(this.count / this.limit), 1)
+    },
+  },
+
+  watch: {
+    orderBy () {
+      this.getPracticeInvoices()
+    },
+
+    limit () {
+      this.page = 1
+      this.getPracticeInvoices()
+    },
+
+    activePage () {
+      this.getPracticeInvoices()
+    },
+  },
+
+  mounted () {
+    const {
+      order_by: orderBy = [],
+      page,
+      date_start: dateStart,
+      date_end: dateEnd,
+    } = this.$route.query
+
+    this.orderBy = orderBy
+    this.activePage = page ? Number.parseInt(page) : 1
+    this.dateStart = dateStart ? dateStart : ''
+    this.dateEnd = dateEnd ? dateEnd : ''
+
+    this.getPracticeInvoices()
+  },
+
+  methods: {
+    filterReset () {
+      this.dateStart = ''
+      this.dateEnd = ''
+
+      this.filterSearch()
+    },
+
+    filterSearch () {
+      this.activePage = 1
+
+      const query = {
+        ...this.$route.query,
+        date_start: this.dateStart ? this.dateStart : undefined,
+        dateEnd: this.dateEnd ? this.dateEnd : undefined,
+        page: undefined,
       }
-    },
 
-    computed: {
-      offset () {
-        return this.activePage * this.limit - this.limit
-      },
-
-      columnDetails () {
-        return [
-          {
-            title: '#',
-            key: 'index',
-            sort_key: null,
-            column: (item, index) => this.offset + index + 1,
-            justify: 'end',
-            flexGrow: 0,
-            flexShrink: 0,
-          },
-          {
-            title: 'Practice',
-            key: 'practice_name',
-            sort_key: 'practice_name',
-            column: (item) => item.practice_name,
-            justify: 'start',
-            flexGrow: 1,
-            flexShrink: 0,
-          },
-          {
-            title: 'Date Start',
-            key: 'date_start',
-            sort_key: 'date_start',
-            column: (item) => this.$moment(item.date_start, 'YYYY-MM-DD').format('DD/MM/YYYY'),
-            justify: 'center',
-            flexGrow: 1,
-            flexShrink: 0,
-          },
-          {
-            title: 'Date End',
-            key: 'date_end',
-            sort_key: 'date_end',
-            column: (item) => this.$moment(item.date_end, 'YYYY-MM-DD').format('DD/MM/YYYY'),
-            justify: 'center',
-            flexGrow: 1,
-            flexShrink: 0,
-          },
-          {
-            title: 'Invoice Number',
-            key: 'invoice_number',
-            sort_key: 'invoice_number',
-            column: (item) => item.invoice_number,
-            justify: 'start',
-            flexGrow: 1,
-            flexShrink: 0,
-          },
-          {
-            title: '£ Amount',
-            key: 'total_amount',
-            sort_key: 'total_amount',
-            column: (item) => item.total_amount ? item.total_amount.toFixed(2) : null,
-            justify: 'end',
-            flexGrow: 1,
-            flexShrink: 0,
-          },
-          {
-            title: 'Hours',
-            key: 'hours',
-            sort_key: 'hours',
-            column: (item) => item.hours ? (parseFloat(item.hours)/60).toFixed(2) + ' Hours' : null,
-            justify: 'end',
-            flexGrow: 1,
-            flexShrink: 0,
-          },
-          {
-            title: 'Discount',
-            key: 'total_credit',
-            sort_key: 'total_credit',
-            column: (item) => item.total_credit ? item.total_credit.toFixed(2) : null,
-            justify: 'end',
-            flexGrow: 1,
-            flexShrink: 0,
-          },
-        ]
-      },
-
-      pages () {
-        return Math.max(Math.ceil(this.count / this.limit), 1)
-      },
-    },
-
-    watch: {
-      orderBy () {
-        this.getPracticeInvoices()
-      },
-
-      limit () {
-        this.page = 1
-        this.getPracticeInvoices()
-      },
-
-      activePage () {
-        this.getPracticeInvoices()
-      },
-    },
-
-    mounted () {      
-      // const {
-   
-      // } = this.$route.query
-
+      if (this.$router.resolve({ query }).href !== this.$route.fullPath) {
+        this.$router.replace({ query })
+      }
       
-      const {
-        order_by: orderBy = [],
-        page,
-        date_start: dateStart,
-        date_end: dateEnd,
-      } = this.$route.query
+      this.getPracticeInvoices()
+    },
 
-      this.orderBy = orderBy
-      this.activePage = page ? Number.parseInt(page) : 1
-      this.dateStart = dateStart ? dateStart : ''
-      this.dateEnd = dateEnd ? dateEnd : ''
+    setPage (page) {
+      this.activePage = page
+
+      if (this.activePage === 1) {
+        this.$router.replace({
+          query: {
+            ...this.$route.query,
+            page: undefined,
+          }
+        })
+      } else {
+        this.$router.replace({
+          query: {
+            ...this.$route.query,
+            page: this.activePage,
+          }
+        })
+      }
 
       this.getPracticeInvoices()
     },
 
-    methods: {
-      filterReset () {
-        this.dateStart = ''
-        this.dateEnd = ''
+    setOrderBy (orderBy) {
+      this.orderBy = orderBy
+      this.activePage = 1
 
-        this.filterSearch()
-      },
-
-      filterSearch () {
-        this.activePage = 1
-
-        const query = {
+      this.$router.replace({
+        query: {
           ...this.$route.query,
-          date_start: this.dateStart ? this.dateStart : undefined,
-          dateEnd: this.dateEnd ? this.dateEnd : undefined,
+          order_by: this.orderBy,
           page: undefined,
         }
+      })
 
-        if (this.$router.resolve({ query }).href !== this.$route.fullPath) {
-          this.$router.replace({ query })
-        }
-        
-        this.getPracticeInvoices()
-      },
+      this.getPracticeInvoices()
+    },
 
-      setPage (page) {
-        this.activePage = page
-
-        if (this.activePage === 1) {
-          this.$router.replace({
-            query: {
-              ...this.$route.query,
-              page: undefined,
-            }
-          })
-        } else {
-          this.$router.replace({
-            query: {
-              ...this.$route.query,
-              page: this.activePage,
-            }
-          })
-        }
-
-        this.getPracticeInvoices()
-      },
-
-      setOrderBy (orderBy) {
-        this.orderBy = orderBy
-        this.activePage = 1
-
-        this.$router.replace({
-          query: {
-            ...this.$route.query,
-            order_by: this.orderBy,
-            page: undefined,
-          }
-        })
-
-        this.getPracticeInvoices()
-      },
-
-      getPracticeInvoices () {
-        this.loading = true
-        this.practiceInvoices = []
-        let params = {
-          practice_id: this.$auth.user.practice_detail.practice.id,
-          date_start: this.dateStart ? this.dateStart : undefined,
-          date_end: this.dateEnd ? this.dateEnd : undefined,
-        }
-        Promise.all([
-          this.$axios.get('/api/v1/admin/reports/practice-invoices/count', {
-            params
-          }).then((responses) => {
-            return responses.data.data.count
-          }),
-          this.$axios.get('/api/v1/admin/reports/practice-invoices', {
-            params: {
-              ...params,
-              order_by: this.orderBy,
-              limit: this.limit,
-              offset: this.offset,
-            },
-          }).then((responses) => {
-            return responses.data.data.practice_invoices
-          }),
-          new Promise((resolve) => setTimeout(resolve, 500))
-        ]).then((results) => {
-          const [
-            count,
-            practiceInvoices,
-          ] = results
-
-          this.count = count
-          this.practiceInvoices = practiceInvoices
-        }).catch((err) => {
-          console.log('err.response ? err.response.data : err', err.response ? err.response.data : err)
-          this.$nuxt.error(err.response ? err.response.data : err)
-        }).finally(() => {
-          this.loading = false
-        })
-      },
-
-      downloadCsv () {
-        this.downloading = true
-        const params = {
-          practice_id: this.$auth.user.practice_detail.practice.id,
-          date_start: this.dateStart ? this.dateStart : undefined,
-          date_end: this.dateEnd ? this.dateEnd : undefined,
-          order_by: this.orderBy,
-          limit: 999,
-          offset: 0,
-        }
-
-        this.$axios.post('/api/v1/admin/reports/practice-invoices/generate-key', {
-          filename: `practiceInvoices.csv`,
-        }, {
+    getPracticeInvoices () {
+      this.loading = true
+      this.practiceInvoices = []
+      let params = {
+        practice_id: this.$auth.user.practice_detail.practice.id,
+        date_start: this.dateStart ? this.dateStart : undefined,
+        date_end: this.dateEnd ? this.dateEnd : undefined,
+      }
+      Promise.all([
+        this.$axios.get('/api/v1/admin/reports/practice-invoices/count', {
+          params
+        }).then((responses) => {
+          return responses.data.data.count
+        }),
+        this.$axios.get('/api/v1/admin/reports/practice-invoices', {
           params: {
             ...params,
-            practice_id: this.$auth.user.practice_detail.practice.id,
+            order_by: this.orderBy,
+            limit: this.limit,
+            offset: this.offset,
           },
         }).then((responses) => {
-          const token = responses.data.data.token
+          return responses.data.data.practice_invoices
+        }),
+        new Promise((resolve) => setTimeout(resolve, 500))
+      ]).then((results) => {
+        const [
+          count,
+          practiceInvoices,
+        ] = results
 
-          window.open(`${process.env.API_URL}/api/v1/admin/reports/practice-invoices/csv?token=${token}`)
-        }).catch((err) => {
-          console.log('err', err)
-          this.$nuxt.error(err.response ? err.response.data : err)
-        }).finally(() => {
-          this.downloading = false
-        })
-      },
+        this.count = count
+        this.practiceInvoices = practiceInvoices
+      }).catch((err) => {
+        console.log('err.response ? err.response.data : err', err.response ? err.response.data : err)
+        this.$nuxt.error(err.response ? err.response.data : err)
+      }).finally(() => {
+        this.loading = false
+      })
     },
-  }
+
+    downloadCsv () {
+      this.downloading = true
+      const params = {
+        practice_id: this.$auth.user.practice_detail.practice.id,
+        date_start: this.dateStart ? this.dateStart : undefined,
+        date_end: this.dateEnd ? this.dateEnd : undefined,
+        order_by: this.orderBy,
+        limit: 999,
+        offset: 0,
+      }
+
+      this.$axios.post('/api/v1/admin/reports/practice-invoices/generate-key', {
+        filename: `practiceInvoices.csv`,
+      }, {
+        params: {
+          ...params,
+          practice_id: this.$auth.user.practice_detail.practice.id,
+        },
+      }).then((responses) => {
+        const token = responses.data.data.token
+
+        window.open(`${process.env.API_URL}/api/v1/admin/reports/practice-invoices/csv?token=${token}`)
+      }).catch((err) => {
+        console.log('err', err)
+        this.$nuxt.error(err.response ? err.response.data : err)
+      }).finally(() => {
+        this.downloading = false
+      })
+    },
+  },
+}
 </script>
