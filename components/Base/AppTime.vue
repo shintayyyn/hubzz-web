515d59fc0f302f5df44df45ad07def365b931d1b
arvi <cacheng.halcyondigital@gmail.com>
2022-04-27 16:50:43 +0800

    ...

--- components/Base/AppTime.vue
+++ components/Base/AppTime.vue
@@ -10,6 +10,7 @@
         <span v-if="required" class="text-red-500">*</span>
       </label>
     </div>
+
     <div class="flex flex-col justify-start">
       <input
         :value="value"
@@ -18,11 +19,12 @@
         class="focus:border-yellow-400 focus:outline-none py-1 font-bold text-xs w-full shadow-none text-center"
         :class="[error ? 'border-red-500' : '', modal === true && 'border-yellow-400', inClass, disabled ? 'cursor-not-allowed text-gray-500' : '', border ? 'border-2 rounded px-2' : 'border-b-2']"
         :style="inStyle"
-        readonly
+        :readonly="readonly"
         :disabled="disabled"
         @click="modal = true"
         @input="$emit('input', $event.target.value)"
         @change="$emit('change', $event.target.value)"
+        @keydown="(e) => keydownHandler(e, value)"
       >
       <transition name="drop-down">
         <div
@@ -33,6 +35,7 @@
         </div>
       </transition>
     </div>
+
     <transition name="fade">
       <div v-if="modal" class="relative z-10 flex justify-center">
         <div class="w-full absolute border bg-white rounded-b-lg shadow-md" style="min-width: 80px">
@@ -40,6 +43,7 @@
             <div class="input-container flex flex-col overflow-y-auto w-full">
               <div
                 v-for="(hour, index) in hours"
+                :ref="`hour-${hour}`"
                 :key="index"
                 class="flex flex-row justify-center cursor-pointer text-sm"
                 :class="{ 'bg-gray-600 text-white': selectedHour === hour }"
@@ -48,9 +52,11 @@
                 {{ hour }}
               </div>
             </div>
+
             <div class="input-container flex flex-col overflow-y-auto w-full">
               <div
-                v-for="(minute, index) in computedMinutes"
+                v-for="(minute, index) in minutes"
+                :ref="`minute-${minute}`"
                 :key="index"
                 class="flex flex-row justify-center cursor-pointer text-sm"
                 :class="{ 'bg-gray-600 text-white': selectedMinute === minute }"
@@ -65,34 +71,10 @@
     </transition>
   </div>
 </template>
+
 <script>
 import { mixin as clickaway, } from "vue-clickaway"
-// const hours = [
-// 	"00",
-// 	"01",
-// 	"02",
-// 	"03",
-// 	"04",
-// 	"05",
-// 	"06",
-// 	"07",
-// 	"08",
-// 	"09",
-// 	"10",
-// 	"11",
-// 	"12",
-// 	"13",
-// 	"14",
-// 	"15",
-// 	"16",
-// 	"17",
-// 	"18",
-// 	"19",
-// 	"20",
-// 	"21",
-// 	"22",
-// 	"23"
-// ];
+
 const minutes = [
   "00",
   "01",
@@ -155,8 +137,10 @@ const minutes = [
   "58",
   "59",
 ]
+
 export default {
   mixins: [clickaway,],
+
   props: {
     value: {
       type: String,
@@ -217,16 +201,21 @@ export default {
       type: Boolean,
       default: false,
     },
+
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
   },
   data () {
     return {
-      // hours: [],
       minutes,
       selectedHour: "00",
       selectedMinute: "00",
       modal: false,
     }
   },
+
   computed: {
     hours () {
       if (this.hourType && ["am", "AM",].includes(this.hourType)) {
@@ -288,24 +277,45 @@ export default {
         ]
       }
     },
-    computedMinutes () {
-      if (this.selectedHour === "24") {
-        return ["00",]
-      } else {
-        return this.minutes
-      }
-    },
   },
+
   watch: {
-    selectedHour (newValue) {
-      if (newValue === "24") {
-        this.selectTime("00", "minute")
-      }
-      this.$emit("change", newValue)
-    },
-    selectedMinute (newValue) {
-      this.$emit("change", newValue)
-    },
+    // selectedHour (hour) {
+    //   console.log('selectedHour', hour)
+    //   // if (hour === "24") {
+    //   //   this.selectTime("00", "minute")
+    //   // }
+
+    //   // this.$emit("change", hour)
+
+    //   console.log('this.$refs', this.$refs[`hour-${hour}`] && this.$refs[`hour-${hour}`][0])
+
+    //   if (this.$refs[`hour-${hour}`] && this.$refs[`hour-${hour}`][0]) {
+    //     console.log('scrollIntoView hour')
+    //     this.$refs[`hour-${hour}`][0].scrollIntoView({
+    //       behavior: "smooth",
+    //       block: "nearest",
+    //       inline: "start",
+    //     })
+    //   }
+    // },
+
+    // selectedMinute (minute) {
+    //   console.log('selectedMinute', minute)
+
+    //   // this.$emit("change", minute)
+
+    //   console.log('this.$refs', this.$refs[`minute-${minute}`] && this.$refs[`minute-${minute}`][0])
+
+    //   if (this.$refs[`minute-${minute}`] && this.$refs[`minute-${minute}`][0]) {
+    //     console.log('scrollIntoView minute')
+    //     this.$refs[`minute-${minute}`][0].scrollIntoView({
+    //       behavior: "smooth",
+    //       block: "nearest",
+    //       inline: "start",
+    //     })
+    //   }
+    // },
 
     value () {
       this.setHourMinuteFromValue()
@@ -317,16 +327,100 @@ export default {
   },
 
   methods: {
+    keydownHandler ($event, value) {
+      const allowedKeys = [
+        'Shift',
+        'Alt',
+        'Control',
+        'Backspace',
+        'Delete',
+        'ArrowLeft',
+        'ArrowRight',
+        'Tab',
+      ]
+
+      if (value?.length === 0) {
+        allowedKeys.push(...[
+          '0',
+          '1',
+          '2',
+        ])
+      }
+
+      if (value?.length === 1) {
+        if (value === '0' || value === '1') {
+          allowedKeys.push(...[
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+          ])
+        }
+
+        if (value === '2') {
+          allowedKeys.push(...[
+            '0',
+            '1',
+            '2',
+            '3',
+          ])
+        }
+      }
+
+      if (value?.length === 2) {
+        allowedKeys.push(...[
+          ':',
+        ])
+      }
+
+      if (value?.length === 3) {
+        allowedKeys.push(...[
+          '0',
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+        ])
+      }
+
+      if (value?.length === 4) {
+        allowedKeys.push(...[
+          '0',
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7',
+          '8',
+          '9',
+        ])
+      }
+
+      if (!allowedKeys.includes($event.key)) {
+        console.log('preventDefault')
+        $event.preventDefault()
+      }
+    },
+
     setHourMinuteFromValue () {
       const hour = this.value && this.value.split(":")[0] ? this.value.split(":")[0] : ''
 
       const minute = this.value && this.value.split(":")[1] ? this.value.split(":")[1] : ''
 
-      if (hour !== this.selectedHour) {
+      if (hour !== this.selectedHour && this.hours.includes(hour)) {
         this.selectedHour = hour
       }
 
-      if (minute !== this.selectedMinute) {
+      if (minute !== this.selectedMinute && this.minutes.includes(minute)) {
         this.selectedMinute = minute
       }
     },
@@ -342,12 +436,14 @@ export default {
 
       this.$emit("input", `${this.selectedHour}:${this.selectedMinute}`)
     },
+    
     toggledOff () {
       this.modal = false
     },
   },
 }
 </script>
+
 <style scoped>
 .input-container {
 	min-height: 100px;
---
+++ pages/apptime.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="h-full w-full flex jusitify-center items-center">
+    <div :style="`width: 120px;`">
+      <AppTime
+        v-model="apptime"
+        :wrapperClass="'mb-1 py-1'"
+        :inStyle="`
+        background-color: transparent;
+      `"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import AppTime from "@/components/Base/AppTime"
+
+export default {
+  layout: "auth",
+
+  components: {
+    AppTime,
+  },
+
+  data () {
+    return {
+      apptime: "",
+    }
+  },
+}
+</script>
