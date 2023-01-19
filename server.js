2fdc79212eafffb481a09150451a47bb6100098a
arvi <cacheng.halcyondigital@gmail.com>
2023-01-19 12:49:26 +0800

    fix: add express to fix static server

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static('dist'))
+
+app.get('/', (req, res) => {
+  res.sendFile('dist/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
