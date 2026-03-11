3633347f0022d60062160066996eb27e11d05aa4
DevJarbs <jarveeviosbelarmino@gmail.com>
2026-03-11 09:23:38 +0800

    fix: paginatin disappearing when selecting All

--- components/Base/AppPagination.vue
+++ components/Base/AppPagination.vue
@@ -29,13 +29,16 @@
             <option v-if="total > 50" :value="50">
               50
             </option>
-            <!--Removed "All" option in pagination-->
+            <option :value="total" selected>
+              All
+            </option>
           </select>
           items
         </div>
       </div>
     </div>
-    <div v-if="totalPages > 1" class="flex flex-col justify-center items-center py-2">
+    <!--change to >=1--> <div v-if="totalPages >= 1" class="flex flex-col justify-center items-center py-2">
+      <!--end-->
       <!-- <div class="flex items-center">
         <div v-for="page in pages" :key="page.name" class="md:hidden pagination-item m-1">
           <button
