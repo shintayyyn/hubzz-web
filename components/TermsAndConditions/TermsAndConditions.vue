df1fdbb74c4d692f060154c5a0bb2ecb66396f2d
DevJarbs <jarveeviosbelarmino@gmail.com>
2026-03-16 16:52:27 +0800

    fix: formatting not working properly

--- components/TermsAndConditions/TermsAndConditions.vue
+++ components/TermsAndConditions/TermsAndConditions.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="rounded-lg border bg-white p-4 my-4 text-sm leading-relaxed">
-    <div v-html="terms.terms_and_conditions" />
+    <div class="ql-editor" v-html="terms.terms_and_conditions" />
   </div>
 </template>
 <script>
 export default {
-  props: ["terms"]
+  props: {
+    terms: {
+      type: Object,
+      default: null
+    }
+  }
 };
 </script>
