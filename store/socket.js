febdd3fbaa1844f8e5694f4f81056dec1e137a51
Shin <shintaine.agilewebworks@gmail.com>
2026-07-02 11:35:11 +0800

    Fix : Sidebar is still visible even after logout, additional

--- store/socket.js
+++ store/socket.js
@@ -1,33 +1,38 @@
-export const state = () => ({
+export const state = () => ({});
 
-})
+export const getters = {};
 
-export const getters = {
-}
-
-export const mutations = {
-}
+export const mutations = {};
 
 export const actions = {
-  async init () {
-    console.log('Socket Initialize')
+  async init() {
+    console.log("Socket Initialize");
 
-    this.$socket.on('connect', () => {
-      console.log('Socket Connected', this.$socket.id, this.$auth.loggedIn)
+    this.$socket.on("connect", () => {
+      console.log("Socket Connected", this.$socket.id, this.$auth.loggedIn);
 
-      let token = this.$auth.$storage.getUniversal('_token.local')
+      let token = this.$auth.$storage.getUniversal("_token.local");
 
-      console.log('Socket Connected', token)
+      console.log("Socket Connected", token);
 
       if (token) {
-        token = token.replace('Bearer ', '')
+        token = token.replace("Bearer ", "");
 
-        this.$axios.post('/api/v1/socket/login', {
-          socket_id: this.$socket.id,
-        }).then(() => {
-          console.log('Socket Logged In')
-        })
+        this.$axios
+          .post("/api/v1/socket/login", {
+            socket_id: this.$socket.id
+          })
+          .then(() => {
+            console.log("Socket Logged In");
+          });
       }
-    })
-  },
-}
+    });
+    this.$socket.on("disconnect", () => {
+      this.$axios
+        .post("/api/v1/socket/logout", {
+          socket_id: this.$socket.id
+        })
+        .catch(() => {});
+    });
+  }
+};
