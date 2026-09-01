43ddc63fa5bf381cfdb132ee30de6063c7324ebf
DevJarbs <jarveeviosbelarmino@gmail.com>
2026-09-01 15:19:20 +0800

    test

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,5 +1,6 @@
 export default function ({store, redirect}) {
-    if (store.$auth.$state.loggedIn) {
-        return redirect('/dashboard')
-    } 
+  if (store.$auth.$state.loggedIn) {
+    return redirect('/dashboard')
+  } 
 }
+//test
